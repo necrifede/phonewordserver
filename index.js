8c40f175162a4ed6ac9ffecd73a49fdb3e03cbfb
Nestor Felipe <necrifede@gmail.com>
2019-04-22 23:59:22 +0200

    Fix #6 eslint rules added

--- index.js
+++ index.js
@@ -3,15 +3,15 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const config = require('./config')
-const app = express();
+const app = express()
 
 app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
 app.get('/phonenumber', (req, res) => {
   return res.status(200).send({ message: 'working...'})
 })
 
 app.listen(config.port, () => {
-  console.log(`Running server in port ${config.port}`)
+  console.log(`Running server in port ${config.port}`) // eslint-disable-line no-console
 })
