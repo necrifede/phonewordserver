82f4f6fde0656704a49f3e53521dfd345f84ba2e
Nestor Felipe <necrifede@gmail.com>
2019-04-23 13:43:48 +0200

    fix #9 adding cors configuration

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 'use strict'
 
 const express = require('express')
+const cors = require('cors')
 const bodyParser = require('body-parser')
 const config = require('./config')
 const phonenumber = require('./phonewords')
 const app = express()
+
+// TODO: Enable All CORS Requests, This is not good, control with CORS options
+app.use(cors())
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
